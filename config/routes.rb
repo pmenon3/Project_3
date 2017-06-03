d535c9e5019033decb19b194a5973006d4e02bbf
Prashob Menon <prashob.menon@chicagobooth.edu>
2017-06-03 16:31:09 -0500

    with users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 
+  devise_for :users
   #here's the root:
   root "histories#index"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170603153808) do
+ActiveRecord::Schema.define(version: 20170603212420) do
 
   create_table "histories", force: :cascade do |t|
     t.string   "pseudoID"
